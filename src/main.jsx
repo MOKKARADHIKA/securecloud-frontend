b6614d6104a4620a36b6dcec6e296aa66f6186d5
MOKKA RADHIKA <mokkaradhika502@gmail.com>
2026-06-23 12:42:10 +0530

    Fix Vite React plugin issue

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-// import { AuthProvider } from "./context/AuthContext";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <AuthProvider>
-//     <App />
-//   </AuthProvider>
-// );
